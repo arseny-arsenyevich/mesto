841b6e8d11bbaef6cdc3499ee699491184e98761
arseny kaptelinin <arseny.kaptelinin@yandex.ru>
2021-01-15 18:41:16 +0300

    aaaaaaaaaa

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
-let editButton = document.querySelector(".profile__edit-button");
-let overlay = document.querySelector(".overlay");
-let exitButton = document.querySelector(".popup__exit");
-let saveButton = document.querySelector(".popup__save-button");
-let travelerName = document.querySelector(".profile__name");
-let travelerProfession = document.querySelector(".profile__profession");
-let travelerNameEdit = document.querySelector(".popup__form_input_name");
-let travelerProfessionEdit = document.querySelector(".popup__form_input_profession");
+let editButton = document.querySelector(".profile__edit-button")
+let overlay = document.querySelector(".overlay")
+let exitButton = document.querySelector(".popup__exit")
+let saveButton = document.querySelector(".popup__save-button")
+let travelerName = document.querySelector(".profile__name")
+let travelerProfession = document.querySelector(".profile__profession")
+let travelerNameEdit = document.querySelector(".popup__form_input_name")
+let travelerProfessionEdit = document.querySelector(".popup__form_input_profession")
 
-travelerNameEdit.value = travelerName.textContent;
-travelerProfessionEdit.value = travelerProfession.textContent;
+travelerNameEdit.value = travelerName.textContent
+travelerProfessionEdit.value = travelerProfession.textContent
 
 let togglePopup = () => {
     overlay.classList.toggle("overlay_active")
